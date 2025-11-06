f106f4eaa8a12a622f7eb332b33fa91a4f5e1974
Matteo Lugli <matteolugli18@gmail.com>
2025-11-06 09:31:20 +0100

    better approach for part 3

--- DeD/q3/c.py
+++ DeD/q3/c.py
@@ -2,11 +2,5 @@ import sys
 filename = sys.argv[1]
 crates = [int(x) for x in open(filename).read().split(",")]
 s = sorted(crates)
-
-counter = 0
-while s:
-    taken = sorted(set(s))
-    for x in taken:
-        s.remove(x)
-    counter += 1
-print(counter)
+counter = {s.count(c) for c in s}
+print(max(counter))
